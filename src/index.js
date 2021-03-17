098ad6b0f4c9e486aca12f225d640e172a24862f
SAGAR KUMAR RANA <s1g1rk1m1r@gmail.com>
2021-03-17 11:26:31 +0530

    this is second change done

--- src/index.js
+++ src/index.js
@@ -13,8 +13,60 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-var heading = document.getElementById("main-head");
+var scrollbtn1 = document.getElementById("scrollbtn1");
+var scrollbtn2 = document.getElementById("scrollbtn2");
+var scrollbtn3 = document.getElementById("scrollbtn3");
+var scrollbtn4 = document.getElementById("scrollbtn4");
+var scrollbtn5 = document.getElementById("scrollbtn5");
 
+var first = document.getElementById('main-head');
+var second = document.getElementById('Second');
+var third = document.getElementById('third');
+var fourth = document.getElementById('fourth');
+var contact = document.getElementById('contact');
+
+
+ //console .log(heading.innerText); 
+ scrollbtn1.addEventListener('click', function(){
+
+   first.style.visibility = "visible";
+   second.style.visibility = "hidden";
+   third.style.visibility = "hidden";
+   fourth.style.visibility = "hidden";
+   contact.style.visibility = "hidden";
+ });
+ scrollbtn2.addEventListener('click', function(){
+
+  first.style.visibility = "hidden";
+  second.style.visibility = "visible";
+  third.style.visibility = "hidden";
+  fourth.style.visibility = "hidden";
+  contact.style.visibility = "hidden";
+});
+scrollbtn3.addEventListener('click', function(){
+
+  first.style.visibility = "hidden";
+  second.style.visibility = "hidden";
+  third.style.visibility = "visible";
+  fourth.style.visibility = "hidden";
+  contact.style.visibility = "hidden";
+});
+scrollbtn4.addEventListener('click', function(){
+
+  first.style.visibility = "hidden";
+  second.style.visibility = "hidden";
+  third.style.visibility = "hidden";
+  fourth.style.visibility = "visible";
+  contact.style.visibility = "hidden";
+});
+scrollbtn5.addEventListener('click', function(){
+
+  first.style.visibility = "hidden";
+  second.style.visibility = "hidden";
+  third.style.visibility = "hidden";
+  fourth.style.visibility = "hidden";
+  contact.style.visibility = "visible";
+});
 
 // heading.addEventListener('click',fun(){
   
